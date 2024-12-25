659fcc1bad38580d31b86077263e7c228ba2ca29
Alif Jawad Mahi <mahi.alifjawad@gmail.com>
2024-12-25 14:49:00 +0600

    Add Signup and Login Features
    Added Sigup and login functionality

--- backend/server.ts
+++ backend/server.ts
@@ -12,105 +12,102 @@ import { getUserAppointments } from "./controllers/AppointmentController";
 import { getVetById, newVet, editVet } from "./controllers/vetController";
 import { signup, getProfileById, login } from "./controllers/userController";
 import {
-	deleteService,
-	editService,
-	getServiceById,
-	getServicesById,
-	newService,
-	searchServices,
+  deleteService,
+  editService,
+  getServiceById,
+  getServicesById,
+  newService,
+  searchServices,
 } from "./controllers/serviceController";
 import {
-	deletePetById,
-	editPet,
-	getPetById,
-	getPets,
-	newPet,
+  deletePetById,
+  editPet,
+  getPetById,
+  getPets,
+  newPet,
 } from "./controllers/petController";
 import {
-	sendOtp,
-	verifyOtp,
-	setNewPassword,
+  sendOtp,
+  verifyOtp,
+  setNewPassword,
 } from "./controllers/otpController";
 
 import { newRate, getRatings } from "./controllers/RateController";
 import {
-	deleteArticle,
-	getArticle,
-	newArticle,
-	getArticleById,
+  deleteArticle,
+  getArticle,
+  newArticle,
+  getArticleById,
 } from "./controllers/ArticleController";
 import {
-	deleteComments,
-	getComments,
-	newComment,
+  deleteComments,
+  getComments,
+  newComment,
 } from "./controllers/CommentController";
 
 const app: Application = express();
 
 app.use(
-	cors({
-		origin: "*",
-		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-		allowedHeaders: ["Content-Type", "Authorization"],
-	})
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization"
-	);
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-	const contentLength = req.headers["content-length"];
-	console.log(
-		`==> \x1b[33m${req.method} ${req.url}\x1b[0m` +
-			" " +
-			`\x1b[36mPayload Size: ${
-				contentLength ? contentLength + " bytes" : "unknown"
-			}\x1b[0m\n`
-	);
-	next();
+  const contentLength = req.headers["content-length"];
+  console.log(
+    `==> \x1b[33m${req.method} ${req.url}\x1b[0m` +
+      " " +
+      `\x1b[36mPayload Size: ${
+        contentLength ? contentLength + " bytes" : "unknown"
+      }\x1b[0m\n`
+  );
+  next();
 });
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_URI!);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.error("MongoDB connection failed:", error);
-		process.exit(1);
-	}
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2024-12-18.acacia",
+  apiVersion: "2024-12-18.acacia",
 });
 
 // API to create payment intent
 app.post("/api/create-payment-intent", async (req, res) => {
-	const { amount } = req.body;
+  const { amount } = req.body;
 
-	try {
-		const paymentIntent = await stripe.paymentIntents.create({
-			amount, // Amount in cents
-			currency: "usd",
-			automatic_payment_methods: { enabled: true },
-		});
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount, // Amount in cents
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    });
 
-		res.status(200).json({
-			clientSecret: paymentIntent.client_secret,
-		});
-	} catch (error: any) {
-		res.status(500).json({ message: error.message });
-	}
+    res.status(200).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 // Define appointment routes
@@ -154,6 +151,6 @@ app.delete("/api/comments", deleteComments);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
-	await connectDB();
-	console.log(`Server running on port ${PORT}`);
+  await connectDB();
+  console.log(`Server running on port ${PORT}`);
 });
